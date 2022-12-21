e20a7ec4c14971b21b6b3ff74343c7029ff0db11
prayatnaaa <tudeprayatna04@gmail.com>
2022-12-21 23:56:58 +0800

    ubah

--- jasaPengiriman.c
+++ jasaPengiriman.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 struct pelanggan {
 
@@ -11,7 +12,6 @@ int telp[13];
 
 struct barang{
 
-int berat;
 int harga;
 int jarak;
 }brg;
@@ -22,227 +22,249 @@ int harga1;
 int harga2;
 } amn;
 
-main();
+struct penerima{
 
-void cobaLagi(){
+char nama[50];
+char alamat[50];
+int telp[13];
+} penerima;
 
-char pilih;
+void dps();
+void badung();
+void singaraja();
+void bangli();
 
-printf("========================================== \n");
-printf("Apakah ingin membuat pemesanan lagi?(y/t)");
-scanf("%s", &pilih);
-fflush(stdin);
+void Domisili(){
 
-if(pilih=='y'){
-main();
-}else if(pilih!='y'){
-printf("Order Akan di Proses! \n");
-printf("========================================== \n");
-}else{
-printf("salah \n");
-}
+    int pilih;
 
-}
+    printf("====================================================\n");
+    printf("||                 D O M I S I L I                ||\n");
+    printf("====================================================\n");
+    printf("|| 1. DENPASAR                                    ||\n");
+    printf("|| 2. BADUNG                                      ||\n");
+    printf("|| 3. SINGARAJA                                   ||\n");
+    printf("|| 4. BANGLI                                      ||\n");
+    printf("====================================================\n");
+    scanf("%d", &pilih);
+    system("clear || cls");
 
-
-float kirimReguler(){
-
-
-if(brg.berat <= 5){
-    brg.harga= (brg.berat * 1000);
-
-}else if(brg.berat>5){
-    int temp= (brg.berat % 5);
-    if(temp==0){
-        brg.harga= (brg.berat/5) * ((brg.berat*5)/brg.berat) * 1000;
-    }else{
-        brg.harga= ((brg.berat/5)+1) * ((brg.berat*5)/brg.berat) * 1000;
+    if(pilih==1){
+        dps();
+        printf("Bayar Rp %d \n", brg.harga);
+    }else if(pilih==2){
+        badung();
+        printf("%d \n", brg.harga);
+    }else if(pilih==3){
+        singaraja();
+        printf("%d \n", brg.harga);
+    }else if(pilih==4){
+        bangli();
+        printf("%d\n", brg.harga);
     }
 }
-}
-float kirimCepat(){
 
-if(brg.berat <= 5){
-    brg.harga= ((brg.berat*5)/ brg.berat * 2000);
 
-}else if(brg.berat>5){
-    int temp= (brg.berat % 5);
-    if(temp==0){
-        brg.harga= (brg.berat/5) * ((brg.berat*5)/brg.berat) * 2000;
+void dps(){
+
+     int pilih;
+     char domisili[]="DENPASAR";
+
+    printf("====================================================\n");
+    printf("|| DOMISILI YANG DITUJU?                          ||\n");
+    printf("||                                                ||\n");
+    printf("|| 1. BADUNG                                      ||\n");
+    printf("|| 2. SINGARAJA                                   ||\n");
+    printf("|| 3. BANGLI                                      ||\n");
+    printf("||                                                ||\n");
+    printf("====================================================\n");
+    scanf("%d", &pilih);
+    system("clear || cls");
+
+    if(pilih==1){
+
+        char tujuan[]="BADUNG";
+        brg.jarak=12;
+        brg.harga= brg.jarak * 2000;
+
+    }else if(pilih==2){
+
+        char tujuan[]="SINGARAJA";
+        brg.jarak=50;
+        brg.harga=brg.jarak * 2000;
+
+    }else if(pilih==3){
+
+        char tujuan[]="BANGLI";
+        brg.jarak= 25;
+        brg.harga=brg.jarak * 2000;
+
     }else{
-        brg.harga= ((brg.berat/5)+1) * ((brg.berat*5)/brg.berat) * 2000;
-    }
 
+        printf(" DATA YANG ANDA MASUKAN TIDAK ADA \n");
+        dps();
+    }
 }
+
+void badung(){
+
+     int pilih;
+     char domisili[]="BADUNG";
+
+    printf("====================================================\n");
+    printf("|| DOMISILI YANG DITUJU?                          ||\n");
+    printf("||                                                ||\n");
+    printf("|| 1. DENPASAR                                    ||\n");
+    printf("|| 2. SINGARAJA                                   ||\n");
+    printf("|| 3. BANGLI                                      ||\n");
+    printf("||                                                ||\n");
+    printf("====================================================\n");
+    scanf("%d", &pilih);
+    system("clear || cls");
+
+    if(pilih==1){
+
+        char tujuan[]="DENPASAR";
+        brg.jarak=12;
+        brg.harga= brg.jarak * 2000;
+
+    }else if(pilih==2){
+
+        char tujuan[]="SINGARAJA";
+        brg.jarak=69;
+        brg.harga=brg.jarak * 2000;
+
+    }else if(pilih==3){
+
+        char tujuan[]="BANGLI";
+        brg.jarak= 36;
+        brg.harga=brg.jarak * 2000;
+
+    }else{
+
+        printf(" DATA YANG ANDA MASUKAN TIDAK ADA \n");
+        badung();
+    }
+}
+
+void singaraja(){
+
+     int pilih;
+     char domisili[]="SINGARAJA";
+
+    printf("====================================================\n");
+    printf("|| DOMISILI YANG DITUJU?                          ||\n");
+    printf("||                                                ||\n");
+    printf("|| 1. DENPASAR                                    ||\n");
+    printf("|| 2. BADUNG                                      ||\n");
+    printf("|| 3. BANGLI                                      ||\n");
+    printf("||                                                ||\n");
+    printf("====================================================\n");
+    scanf("%d", &pilih);
+    system("clear || cls");
+
+    if(pilih==1){
+
+        char tujuan[]="DENPASAR";
+        brg.jarak=50;
+        brg.harga= brg.jarak * 2000;
+
+    }else if(pilih==2){
+
+        char tujuan[]="BADUNG";
+        brg.jarak=69;
+        brg.harga=brg.jarak * 2000;
+
+    }else if(pilih==3){
+
+        char tujuan[]="BANGLI";
+        brg.jarak= 73;
+        brg.harga=brg.jarak * 2000;
+
+    }else{
+
+        printf(" DATA YANG ANDA MASUKAN TIDAK ADA \n");
+        singaraja();
+    }
+}
+
+void bangli(){
+
+     int pilih;
+     char domisili[]="BANGLI";
+
+    printf("====================================================\n");
+    printf("|| DOMISILI YANG DITUJU?                          ||\n");
+    printf("||                                                ||\n");
+    printf("|| 1. DENPASAR                                    ||\n");
+    printf("|| 2. BADUNG                                      ||\n");
+    printf("|| 3. SINGARAJA                                   ||\n");
+    printf("||                                                ||\n");
+    printf("====================================================\n");
+    scanf("%d", &pilih);
+    system("clear || cls");
+
+    if(pilih==1){
+
+        char tujuan[]="DENPASAR";
+        brg.jarak=40;
+        brg.harga= brg.jarak * 2000;
+
+    }else if(pilih==2){
+
+        char tujuan[]="BADUNG";
+        brg.jarak=34;
+        brg.harga=brg.jarak * 2000;
+
+    }else if(pilih==3){
+
+        char tujuan[]="SINGARAJA";
+        brg.jarak= 73;
+        brg.harga=brg.jarak * 2000;
+
+    }else{
+
+        printf(" DATA YANG ANDA MASUKAN TIDAK ADA \n");
+        bangli();
+    }
+}
+
+
+
+void dataPenerima(){
+
+    printf("====================================================\n");
+    printf("||            D A T A   P E N E R I M A           ||\n");
+    printf("====================================================\n");
+    printf(" Nama Penerima: ");
+    scanf("%[ \n]", &penerima.nama);
+    fflush(stdin);
+    printf("\n");
+    printf(" Nomor Telepon: ");
+    scanf("%d", penerima.telp);
+    fflush(stdin);
+    system("clear || cls");
+
 }
 
 
 void dataPelanggan(){
 
-printf("=============================== \n");
-printf("              DATA              \n");
-printf("=============================== \n");
-printf("Nama: ");
-scanf("%[^ \n]", pel.nama);
-fflush(stdin);
-printf("Alamat: ");
-scanf("%[^ \n]", pel.alamat);
-fflush(stdin);
-printf("No. Hp: ");
-scanf("%d", pel.telp);
-system("clear || cls");
-}
 
-float pengaman(){
-
-int pilih;
-int hasil;
-
-printf("=============================== \n");
-printf("           PENGAMANAN           \n");
-printf("=============================== \n");
-printf("1. Biasa \n");
-printf("2. Aman \n");
-printf("Pilih Pengamanan: ");
-scanf("%d", &pilih);
-system("clear || cls");
-fflush(stdin);
-
-switch(pilih){
-
-    case 1:
-    amn.harga1=brg.harga + 2000;
+    printf("====================================================\n");
+    printf("||              S E N D E R    D A T A            ||\n");
+    printf("====================================================\n");
+    printf(" Order Atas Nama: ");
+    scanf("%[^ \n]", pel.nama);
     fflush(stdin);
-    break;
-
-    case 2:
-    amn.harga1= brg.harga + 5000;
+    printf("\n");
+    printf("Nomor Telpon: ");
+    scanf("%d", pel.telp);
     fflush(stdin);
-    break;
-
-    default:
-    printf("salah \n");
-    pengaman();
-    fflush(stdin);
-    break;
-
-}
-
-}
-
-float jarak(){
-
-int pilih;
-printf("======================================= \n");
-printf("Masukan Estimasi Jarak Pengiriman (km): ");
-scanf("%d", &brg.jarak);
-system("clear || cls");
-
-if(brg.jarak<=5){
-
-amn.harga2= amn.harga1 + 3000;
-fflush(stdin);
-
-}else if(brg.jarak>5){
-    int temp= (pilih % 5);
-    if(temp==0){
-        amn.harga2= amn.harga1 + 15000;
-        fflush(stdin);
-    }else{
-        amn.harga2= amn.harga1+ 15000;
-        fflush(stdin);
-    }
-}
-
-}
-
-void metodeBayar(){
-
-int pilih;
-int hasil;
-
-printf("=============================== \n");
-printf("         METODE BAYAR           \n");
-printf("=============================== \n");
-printf("1. Tunai \n");
-printf("2. Debit \n");
-printf("3. Pay Later \n");
-printf("Masukan Pilihan: ");
-scanf("%d", &pilih);
-
-switch(pilih){
-
-    case 1:
-    printf("Silahkan melakukan pembayaran sebesar Rp %d \n", amn.harga2);
-    break;
-
-    case 2:
-    printf("Silahkan melakukan pembayaran sebesar Rp %d ke No. rekening 0403288664 \n", amn.harga2);
-    break;
-
-    case 3:
-    hasil= amn.harga2 / 4;
-    printf("Lunaskan dalam 4 bulan dengan membayar Rp %d /bulan \n", hasil);
-    break;
-
-    default:
-    printf("salah \n");
-    metodeBayar();
-    
-}
-
-
-}
-
-void pengiriman(){
-
-int pilih;
-
-printf("=============================== \n");
-printf("           PENGIRIMAN           \n");
-printf("=============================== \n");
-printf("1. Reguler \n");
-printf("2. Cepat \n");
-printf("Pilih: ");
-scanf("%d", &pilih);
-fflush(stdin);
-system("clear || cls");
-
-switch(pilih){
-
-case 1:
-kirimReguler();
-pengaman();
-jarak();
-metodeBayar();
-cobaLagi();
-break;
-
-case 2:
-kirimCepat();
-pengaman();
-jarak();
-metodeBayar();
-cobaLagi();
-break;
-
-default:
-pengiriman();
-break;
-}
+    system("clear || cls");
 }
 
 int main(){
-
-dataPelanggan();
-
-
-printf("=============================== \n");
-printf("Berat barang (kg): ");
-scanf("%d", &brg.berat);
-system("clear || cls");
-
-pengiriman();
-
-return 0;
+    
+    Domisili();
 }
